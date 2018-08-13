94242a0493fba6279f0d44dcb52188c8cca6b8c7
Redhat <redhat@localhost.localdomain>
2018-08-13 16:22:33 +0530

    Adds support for version notifications
    Signed-off-by: Geetika Batra <gbatra@redhat.com>

--- types/type.go
+++ types/type.go
@@ -3,14 +3,15 @@ package types
 type NotificationType string
 
 const (
-	WorkitemCreate       NotificationType = "workitem.create"
-	WorkitemUpdate       NotificationType = "workitem.update"
-	CommentCreate        NotificationType = "comment.create"
-	CommentUpdate        NotificationType = "comment.update"
-	UserEmailUpdate      NotificationType = "user.email.update"
-	InvitationTeamNoorg  NotificationType = "invitation.team.noorg"
-	InvitationSpaceNoorg NotificationType = "invitation.space.noorg"
-	AnalyticsNotifyCVE   NotificationType = "analytics.notify.cve"
+	WorkitemCreate       	 NotificationType = "workitem.create"
+	WorkitemUpdate       	 NotificationType = "workitem.update"
+	CommentCreate        	 NotificationType = "comment.create"
+	CommentUpdate        	 NotificationType = "comment.update"
+	UserEmailUpdate      	 NotificationType = "user.email.update"
+	InvitationTeamNoorg  	 NotificationType = "invitation.team.noorg"
+	InvitationSpaceNoorg 	 NotificationType = "invitation.space.noorg"
+	AnalyticsNotifyCVE   	 NotificationType = "analytics.notify.cve"
+	AnalyticsNotifyVersion   NotificationType = "analytics.notify.version"
 )
 
 var notifiers = map[NotificationType][]string{
